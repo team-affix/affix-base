3a88cac505ee3f3a48cb37274bed14d3dbda3814
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2021-06-14 17:15:52 -0700

    Revert "Remade ptr<T> Class Template"
    This reverts commit e5136233e7b4974bb86c4e44f953b3d9d8e0609f.

--- affix-base/ptr.h
+++ affix-base/ptr.h
@@ -5,89 +5,34 @@ using std::shared_ptr;
 
 namespace affix_base {
 	namespace data {
-
-		class ptr_base {
+		template<class T>
+		class ptr : public shared_ptr<T> {
 		public:
-			ptr_base* m_next = nullptr;
-			ptr_base* m_prev = nullptr;
-		};
-
-		template<typename T>
-		class ptr : public ptr_base {
-		protected:
-			T** raw_ptr = nullptr;
-
-		public:
-			virtual ~ptr() {
-				if (m_prev != nullptr || m_next != nullptr) {
-					unlink();
-				}
-				else {
-					delete* raw_ptr;
-					delete raw_ptr;
-					raw_ptr = nullptr;
-				}
-			}
 			ptr() {
 
 			}
-			ptr(T* a_raw_ptr) {
-				raw_ptr = new T * (a_raw_ptr);
-			}
-			void operator=(T* a_raw_ptr) {
-				raw_ptr = new T * (a_raw_ptr);
-			}
-			void operator=(ptr<T> a_other) {
-				link(a_other);
-			}
-			ptr(const ptr<T>& a_other) {
-				link(const_cast<ptr<T>&>(a_other));
-			}
-			template<typename J>
-			ptr(const ptr<J>& a_other) {
-				link(const_cast<ptr<T>&>(a_other));
+			ptr(T* a) : shared_ptr<T>(a) {
+
 			}
 
 		public:
-			T**& get_raw() {
-				return raw_ptr;
-			}
-			T*& get() {
-				return *raw_ptr;
-			}
 			T& val() {
-				return *get();
+				return *shared_ptr<T>::get();
 			}
-			/*operator T* () {
-				return get();
-			}*/
-			T* operator->() {
-				return get();
+			operator T* () {
+				return shared_ptr<T>::get();
 			}
-			T& operator*() {
-				return val();
-			}
-
 
 		public:
-			template<typename J>
-			void link(ptr<J>& a_other) {
-				m_prev = &a_other;
-				if (a_other.m_next != nullptr)
-					a_other.m_next->m_prev = this;
-				a_other.m_next = this;
-				raw_ptr = (T**)a_other.get_raw();
+			void link(ptr<T>& other) {
+				*this = other;
 			}
 			void unlink() {
-				if (m_prev != nullptr)
-					m_prev->m_next = m_next;
-				if (m_next != nullptr)
-					m_next->m_prev = m_prev;
-				m_prev = nullptr;
-				m_next = nullptr;
+				T temp = val();
+				shared_ptr<T>::reset(new T());
+				val() = temp;
 			}
 
 		};
-
 	}
 }
