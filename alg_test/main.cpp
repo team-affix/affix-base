5ecfc5083921868665117ae917e23769d1f950cc
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2021-09-23 19:23:07 -0700

    Updated threading.h

--- alg_test/main.cpp
+++ alg_test/main.cpp
@@ -162,89 +162,103 @@ ptr<simple_tcp_server> prime_tcp_server(io_context& a_context) {
 
 }
 
+struct sleeper {
+	void sleep() {
+		for (int i = 0; i < 10; i++) {
+			std::cout << "Sleeping " << i << std::endl;
+			Sleep(1000);
+		}
+	}
+};
+
 int main() {
 
 	using namespace affix_base::networking;
 
-	io_context l_server_context;
-	ptr<simple_tcp_server> l_server(prime_tcp_server(l_server_context));
-	std::thread l_server_context_thread([&] { l_server_context.run(); });
+	//io_context l_server_context;
+	//ptr<simple_tcp_server> l_server(prime_tcp_server(l_server_context));
+	//std::thread l_server_context_thread([&] { l_server_context.run(); });
 
-	io_context l_context;
-	tcp::socket l_socket(l_context);
+	//io_context l_context;
+	//tcp::socket l_socket(l_context);
 
-	l_socket.connect(tcp::endpoint(ip::make_address("192.168.1.141"), 8091));
+	//l_socket.connect(tcp::endpoint(ip::make_address("192.168.1.141"), 8091));
 
-	socket_io_guard l_guard(l_socket);
+	//socket_io_guard l_guard(l_socket);
 
-	vector<uint8_t> l_data_0({ 0 });
-	vector<uint8_t> l_data_1({ 1, 1 });
-	vector<uint8_t> l_data_2({ 2, 1, 2 });
-	vector<uint8_t> l_data_3({ 3, 1, 2, 3 });
-	vector<uint8_t> l_data_4({ 4, 1, 2, 3, 4 });
-	vector<uint8_t> l_data_5({ 5, 1, 2, 3, 4, 5 });
+	//vector<uint8_t> l_data_0({ 0 });
+	//vector<uint8_t> l_data_1({ 1, 1 });
+	//vector<uint8_t> l_data_2({ 2, 1, 2 });
+	//vector<uint8_t> l_data_3({ 3, 1, 2, 3 });
+	//vector<uint8_t> l_data_4({ 4, 1, 2, 3, 4 });
+	//vector<uint8_t> l_data_5({ 5, 1, 2, 3, 4, 5 });
 
-	l_guard.async_send(l_data_0, [](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 0" << std::endl;
-	});
-	l_guard.async_send(l_data_1, [](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 1" << std::endl;
-	});
-	l_guard.async_send(l_data_2, [](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 2" << std::endl;
-	});
-	l_guard.async_send(l_data_3, [](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 3" << std::endl;
-	});
-	l_guard.async_send(l_data_4, [](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 4" << std::endl;
-	});
-	l_guard.async_send(l_data_5, [](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 5" << std::endl;
-	});
+	//l_guard.async_send(l_data_0, [](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 0" << std::endl;
+	//});
+	//l_guard.async_send(l_data_1, [](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 1" << std::endl;
+	//});
+	//l_guard.async_send(l_data_2, [](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 2" << std::endl;
+	//});
+	//l_guard.async_send(l_data_3, [](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 3" << std::endl;
+	//});
+	//l_guard.async_send(l_data_4, [](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 4" << std::endl;
+	//});
+	//l_guard.async_send(l_data_5, [](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 5" << std::endl;
+	//});
 
 
-	/*socket_async_send(l_socket, l_data_0, [&](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 0" << std::endl;
-	});
-	socket_async_send(l_socket, l_data_1, [&](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 1" << std::endl;
-	});
-	socket_async_send(l_socket, l_data_2, [&](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 2" << std::endl;
-	});
-	socket_async_send(l_socket, l_data_3, [&](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 3" << std::endl;
-	});
-	socket_async_send(l_socket, l_data_4, [&](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 4" << std::endl;
-	});
-	socket_async_send(l_socket, l_data_5, [&](bool a_result) {
-		lock_guard<mutex> l_lock(g_cout_mutex);
-		std::cout << "Sent data 5" << std::endl;
-	});*/
+	///*socket_async_send(l_socket, l_data_0, [&](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 0" << std::endl;
+	//});
+	//socket_async_send(l_socket, l_data_1, [&](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 1" << std::endl;
+	//});
+	//socket_async_send(l_socket, l_data_2, [&](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 2" << std::endl;
+	//});
+	//socket_async_send(l_socket, l_data_3, [&](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 3" << std::endl;
+	//});
+	//socket_async_send(l_socket, l_data_4, [&](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 4" << std::endl;
+	//});
+	//socket_async_send(l_socket, l_data_5, [&](bool a_result) {
+	//	lock_guard<mutex> l_lock(g_cout_mutex);
+	//	std::cout << "Sent data 5" << std::endl;
+	//});*/
 
-	std::thread l_context_thread([&] { l_context.run(); });
-	Sleep(3000);
+	//std::thread l_context_thread([&] { l_context.run(); });
+	//Sleep(3000);
 
-	l_server_context.stop();
-	l_context.stop();
+	//l_server_context.stop();
+	//l_context.stop();
 
-	if (l_server_context_thread.joinable())
-		l_server_context_thread.join();
-	if (l_context_thread.joinable())
-		l_context_thread.join();
+	//if (l_server_context_thread.joinable())
+	//	l_server_context_thread.join();
+	//if (l_context_thread.joinable())
+	//	l_context_thread.join();
+
+	using namespace affix_base::threading;
+	using namespace affix_base::callback;
+
+
 
 	return EXIT_SUCCESS;
 
