4728f0a3d0d1ee8facf7d9da86fa9ed49385cf5f
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2022-02-21 17:33:53 -0800

    Removed 'dispatched()' function from dispatcher as it was not thread-safe

--- affix-base/dispatcher.h
+++ affix-base/dispatcher.h
@@ -123,23 +123,11 @@ namespace affix_base
 			/// Returns the number of dispatches currently active.
 			/// </summary>
 			/// <returns></returns>
-			size_t dispatched_count(
+			affix_base::threading::locked_resource<size_t, affix_base::threading::cross_thread_mutex> dispatched_count(
 
 			)
 			{
-				affix_base::threading::locked_resource<size_t, MUTEX_TYPE> l_dispatched_count = m_dispatched_count.lock();
-				return l_dispatched_count.resource();
-			}
-
-			/// <summary>
-			/// Returns a boolean describing whether there are any active dispatches.
-			/// </summary>
-			/// <returns></returns>
-			bool dispatched(
-
-			)
-			{
-				return dispatched_count() > 0;
+				return m_dispatched_count.lock();
 			}
 
 		};
