70e248b392466a44dd67e4996236df4531a46dbc
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2022-09-12 00:25:00 -0700

    Cleaned up byte_buffer by removing push_front and pop_back functions

--- affix-base/byte_buffer.h
+++ affix-base/byte_buffer.h
@@ -45,48 +45,7 @@ namespace affix_base {
 				}
 				else
 				{
-					byte_buffer l_byte_buffer;
-					if (!a_data.serialize(l_byte_buffer))
-						return false;
-					return push_back(l_byte_buffer);
-				}
-			}
-			template<typename DATA_TYPE>
-			bool push_front(const DATA_TYPE& a_data) {
-				if constexpr (std::is_standard_layout<DATA_TYPE>::value)
-				{
-					// PUSH DATA TO FRONT OF STACK
-					uint8_t* l_data_begin = (uint8_t*)&a_data;
-					uint8_t* l_data_end = l_data_begin + sizeof(DATA_TYPE);
-					m_body.insert(m_body.begin(), l_data_begin, l_data_end);
-					return true;
-				}
-				else
-				{
-					byte_buffer l_byte_buffer;
-					if (!a_data.serialize(l_byte_buffer))
-						return false;
-					return push_front(l_byte_buffer);
-				}
-			}
-			template<typename DATA_TYPE>
-			bool pop_back(DATA_TYPE& a_data) {
-				if constexpr (std::is_standard_layout<DATA_TYPE>::value)
-				{
-					if (m_body.size() < sizeof(DATA_TYPE)) return false;
-
-					// POP DATA FROM BACK OF STACK
-					std::copy(m_body.end() - sizeof(DATA_TYPE), m_body.end(), (uint8_t*)&a_data);
-					m_body.erase(m_body.end() - sizeof(DATA_TYPE), m_body.end());
-
-					return true;
-				}
-				else
-				{
-					byte_buffer l_byte_buffer;
-					if (!pop_back(l_byte_buffer))
-						return false;
-					return a_data.deserialize(l_byte_buffer);
+					return a_data.serialize(*this);
 				}
 			}
 			template<typename DATA_TYPE>
@@ -103,10 +62,7 @@ namespace affix_base {
 				}
 				else
 				{
-					byte_buffer l_byte_buffer;
-					if (!pop_front(l_byte_buffer))
-						return false;
-					return a_data.deserialize(l_byte_buffer);
+					return a_data.deserialize(*this);
 				}
 			}
 
@@ -119,42 +75,6 @@ namespace affix_base {
 						[&](const auto& a_field)
 						{
 							if (!push_back(a_field))
-								throw std::exception();
-						}, a_data...);
-					return true;
-				}
-				catch (std::exception)
-				{
-					return false;
-				}
-			}
-			template<typename ... DATA_TYPES>
-			bool push_front(const DATA_TYPES& ... a_data)
-			{
-				try
-				{
-					constexpr_for(
-						[&](const auto& a_field)
-						{
-							if (!push_front(a_field))
-								throw std::exception();
-						}, a_data...);
-					return true;
-				}
-				catch (std::exception)
-				{
-					return false;
-				}
-			}
-			template<typename ... DATA_TYPES>
-			bool pop_back(DATA_TYPES& ... a_data)
-			{
-				try
-				{
-					constexpr_for(
-						[&](auto& a_field)
-						{
-							if (!pop_back(a_field))
 								throw std::exception();
 						}, a_data...);
 					return true;
@@ -198,37 +118,6 @@ namespace affix_base {
 				return true;
 			}
 			template<>
-			bool push_front<std::string>(const std::string& a_data)
-			{
-				// Push the size onto the stack twice, once before
-				if (!push_front(a_data.size())) return false;
-
-				for (int i = a_data.size() - 1; i >= 0; i--)
-					if (!push_front(a_data[i])) return false;
-
-				// and once after
-				if (!push_front(a_data.size())) return false;
-
-				return true;
-			}
-			template<>
-			bool pop_back<std::string>(std::string& a_data)
-			{
-				size_t l_size;
-				
-				if (!pop_back(l_size)) return false;
-				
-				a_data.resize(l_size);
-
-				for (int i = l_size - 1; i >= 0; i--)
-					if (!pop_back(a_data[i])) return false;
-
-				if (!pop_back(l_size)) return false;
-
-				return true;
-
-			}
-			template<>
 			bool pop_front<std::string>(std::string& a_data)
 			{
 				size_t l_size;
@@ -245,6 +134,7 @@ namespace affix_base {
 				return true;
 
 			}
+
 			template<typename DATA_TYPE>
 			bool push_back(const std::vector<DATA_TYPE>& a_vec) {
 				
@@ -257,39 +147,6 @@ namespace affix_base {
 
 				// STORE SIZE OF VECTOR AT END (AS WELL) IN CASE OF POP_BACK CALL
 				if (!push_back(uint32_t(a_vec.size()))) return false;
-
-				return true;
-			}
-			template<typename DATA_TYPE>
-			bool push_front(const std::vector<DATA_TYPE>& a_vec) {
-
-				// STORE SIZE OF VECTOR AT END IN CASE OF POP_FRONT CALL
-				if (!push_front(uint32_t(a_vec.size()))) return false;
-
-				// STORE DATA IN FRONT-FACING ORDER ALWAYS
-				for (int i = a_vec.size() - 1; i >= 0; i--)
-					if (!push_front(a_vec[i])) return false;
-
-				// STORE SIZE OF VECTOR AT FRONT (AS WELL) IN CASE OF POP_BACK CALL
-				if (!push_front(uint32_t(a_vec.size()))) return false;
-
-				return true;
-			}
-			template<typename DATA_TYPE>
-			bool pop_back(std::vector<DATA_TYPE>& a_vec) {
-
-				// POP SIZE OF VECTOR FROM BACK
-				uint32_t vec_size = 0;
-				if (!pop_back(vec_size)) return false;
-				a_vec.resize(vec_size);
-
-				// POP ELEMENTS IN FRONT-FACING ORDER
-				for (int i = vec_size - 1; i >= 0; i--) {
-					if (!pop_back(a_vec[i])) return false;
-				}
-
-				// REMOVE THE POP_FRONT SIZE
-				if (!pop_back(vec_size)) return false;
 
 				return true;
 			}
@@ -311,30 +168,13 @@ namespace affix_base {
 
 				return true;
 			}
+			
 			template<typename DATA_TYPE_1, typename DATA_TYPE_2>
 			bool push_back(const std::pair<DATA_TYPE_1, DATA_TYPE_2>& a_tuple) {
 
 				// ALWAYS PUSH TUPLE IN FRONT-FACING ORDER
 				if (!push_back(a_tuple.first)) return false;
 				if (!push_back(a_tuple.second)) return false;
-
-				return true;
-			}
-			template<typename DATA_TYPE_1, typename DATA_TYPE_2>
-			bool push_front(const std::pair<DATA_TYPE_1, DATA_TYPE_2>& a_tuple) {
-
-				// ALWAYS PUSH TUPLE IN FRONT-FACING ORDER
-				if (!push_front(a_tuple.second)) return false;
-				if (!push_front(a_tuple.first)) return false;
-
-				return true;
-			}
-			template<typename DATA_TYPE_1, typename DATA_TYPE_2>
-			bool pop_back(std::pair<DATA_TYPE_1, DATA_TYPE_2>& a_tuple) {
-
-				// ALWAYS POP TUPLE IN FRONT-FACING ORDER
-				if (!pop_back(a_tuple.second)) return false;
-				if (!pop_back(a_tuple.first)) return false;
 
 				return true;
 			}
@@ -364,36 +204,6 @@ namespace affix_base {
 				return true;
 			}
 			template<typename KEY_TYPE, typename VALUE_TYPE>
-			bool push_front(const std::map<KEY_TYPE, VALUE_TYPE>& a_map)
-			{
-				std::vector<std::pair<KEY_TYPE, VALUE_TYPE>> l_vector;
-
-				for (typename std::map<KEY_TYPE, VALUE_TYPE>::const_iterator l_it = a_map.begin(); l_it != a_map.end(); l_it++)
-				{
-					l_vector.push_back(*l_it);
-				}
-
-				if (!push_front(l_vector))
-					return false;
-
-				return true;
-			}
-			template<typename KEY_TYPE, typename VALUE_TYPE>
-			bool pop_back(std::map<KEY_TYPE, VALUE_TYPE>& a_map)
-			{
-				a_map.clear();
-
-				std::vector<std::pair<KEY_TYPE, VALUE_TYPE>> l_vector;
-				
-				if (!pop_back(l_vector))
-					return false;
-
-				for (int i = 0; i < l_vector.size(); i++)
-					a_map.insert(a_map.end(), l_vector[i]);
-
-				return true;
-			}
-			template<typename KEY_TYPE, typename VALUE_TYPE>
 			bool pop_front(std::map<KEY_TYPE, VALUE_TYPE>& a_map)
 			{
 				a_map.clear();
@@ -412,18 +222,6 @@ namespace affix_base {
 			bool push_back(const byte_buffer& a_byte_buffer)
 			{
 				return push_back(a_byte_buffer.data());
-			}
-			bool push_front(const byte_buffer& a_byte_buffer)
-			{
-				return push_front(a_byte_buffer.data());
-			}
-			bool pop_back(byte_buffer& a_byte_buffer)
-			{
-				std::vector<uint8_t> l_byte_buffer_data;
-				if (!pop_back(l_byte_buffer_data))
-					return false;
-				a_byte_buffer = byte_buffer(l_byte_buffer_data);
-				return true;
 			}
 			bool pop_front(byte_buffer& a_byte_buffer)
 			{
@@ -450,42 +248,6 @@ namespace affix_base {
 				return true;
 
 			}
-
-			template<typename DATA_TYPE>
-			bool push_front(
-				const affix_base::data::tree<DATA_TYPE>& a_tree
-			)
-			{
-				const std::vector<affix_base::data::tree<DATA_TYPE>>& l_subtrees(a_tree);
-
-				if (!push_front(l_subtrees))
-					return false;
-
-				if (!push_front(a_tree.m_resource))
-					return false;
-
-				return true;
-
-			}
-
-			template<typename DATA_TYPE>
-			bool pop_back(
-				affix_base::data::tree<DATA_TYPE>& a_tree
-			)
-			{
-
-				std::vector<affix_base::data::tree<DATA_TYPE>>& l_subtrees(a_tree);
-
-				if (!pop_back(l_subtrees))
-					return false;
-
-				if (!pop_back(a_tree.m_resource))
-					return false;
-
-				return true;
-
-			}
-
 			template<typename DATA_TYPE>
 			bool pop_front(
 				affix_base::data::tree<DATA_TYPE>& a_tree
--- alg_test/main.cpp
+++ alg_test/main.cpp
@@ -18,6 +18,35 @@ using std::string;
 namespace ip = asio::ip;
 using asio::error_code;
 
+struct my_serializable_struct : public affix_base::data::serializable
+{
+	std::string m_string = "";
+	int m_int = 0;
+	std::vector<double> m_vector = { };
+
+	my_serializable_struct(
+
+	) :
+		affix_base::data::serializable(m_string, m_int, m_vector)
+	{
+
+	}
+
+	my_serializable_struct(
+		const std::string& a_string,
+		const int& a_int,
+		const std::vector<double>& a_vector
+	) :
+		affix_base::data::serializable(m_string, m_int, m_vector),
+		m_string(a_string),
+		m_int(a_int),
+		m_vector(a_vector)
+	{
+
+	}
+
+};
+
 int main(
 
 )
@@ -28,7 +57,15 @@ int main(
 	namespace fs = std::filesystem;
 	using namespace affix_base::distributed_computing;
 
+	affix_base::data::byte_buffer l_byte_buffer;
 
+	my_serializable_struct l_struct("testing123", 13, { 1.2, 2.3, 3.4, 4.5 });
+
+	l_byte_buffer.push_back(l_struct);
+
+	my_serializable_struct l_struct_2;
+
+	l_byte_buffer.pop_front(l_struct_2);
 
  	return EXIT_SUCCESS;
 
